275aca85fd9be0fea92e20c729bd7292592e6e7a
Julian Tita <juliantita_2003@hotmail.com>
2024-08-12 19:54:09 -0300

    update navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
- 
+
 
 export default function Navbar() {
 
@@ -7,18 +7,24 @@ export default function Navbar() {
         <div className="flex justify-around items-center w-screen bg-emerald-500">
             <div>
 
-            <img src="frontend\src\assets\optica_colon-removebg-preview.png" alt="elpeep" />
+                <img className="p-2 w-32 h-auto max-w-full rounded-lg " src="../src/assets/optica_colon-removebg-preview.png" alt="elpeep" />
 
             </div>
             <div className="">
 
                 <form action="">
 
-                    <div className="flex p-4">
-                        <span role="status" aria-live="polite" className="ui-helper-hidden-accessible"></span>
-                        <input className="rounded-md p-1" type="text" placeholder="Buscar productos" />
-                        <div className="">
+                    <div className="flex bg-white rounded-md ">
+
+                        <input className="rounded-md ml-2" type="text" placeholder="Buscar productos" />
+
+                        <div className="flex">
+
                             <button className="" type="submit" aria-label="Buscar en">
+
+                                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="10" height="10" viewBox="0 0 26 26">
+                                    <path d="M 10 0.1875 C 4.578125 0.1875 0.1875 4.578125 0.1875 10 C 0.1875 15.421875 4.578125 19.8125 10 19.8125 C 12.289063 19.8125 14.394531 19.003906 16.0625 17.6875 L 16.9375 18.5625 C 16.570313 19.253906 16.699219 20.136719 17.28125 20.71875 L 21.875 25.34375 C 22.589844 26.058594 23.753906 26.058594 24.46875 25.34375 L 25.34375 24.46875 C 26.058594 23.753906 26.058594 22.589844 25.34375 21.875 L 20.71875 17.28125 C 20.132813 16.695313 19.253906 16.59375 18.5625 16.96875 L 17.6875 16.09375 C 19.011719 14.421875 19.8125 12.300781 19.8125 10 C 19.8125 4.578125 15.421875 0.1875 10 0.1875 Z M 10 2 C 14.417969 2 18 5.582031 18 10 C 18 14.417969 14.417969 18 10 18 C 5.582031 18 2 14.417969 2 10 C 2 5.582031 5.582031 2 10 2 Z M 4.9375 7.46875 C 4.421875 8.304688 4.125 9.289063 4.125 10.34375 C 4.125 13.371094 6.566406 15.8125 9.59375 15.8125 C 10.761719 15.8125 11.859375 15.433594 12.75 14.8125 C 12.511719 14.839844 12.246094 14.84375 12 14.84375 C 8.085938 14.84375 4.9375 11.695313 4.9375 7.78125 C 4.9375 7.675781 4.933594 7.574219 4.9375 7.46875 Z"></path>
+                                </svg>
 
                             </button>
                         </div>
@@ -27,12 +33,12 @@ export default function Navbar() {
 
                 </form>
 
-                
+
             </div>
 
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-              </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+            </svg>
 
         </div>
     )
